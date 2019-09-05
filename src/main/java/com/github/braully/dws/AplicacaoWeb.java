e1d225c3e5bb900b9b6fc5aa7c9f50e24f53764a
Aluno <Aluno@PC-43693>
2019-09-04 21:30:05 -0300

    Oitava aula

--- src/main/java/com/github/braully/dws/AplicacaoWeb.java
+++ src/main/java/com/github/braully/dws/AplicacaoWeb.java
@@ -11,18 +11,65 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.context.ServletContextAware;
 
 /**
  *
  * @author braully
  */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
-public class AplicacaoWeb implements ServletContextAware {
+public class AplicacaoWeb extends WebSecurityConfigurerAdapter implements ServletContextAware {
 
     public static void main(String... args) {
         SpringApplication.run(AplicacaoWeb.class, args);
     }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("usuario")
+                .password(passwordEncoder().encode("123"))
+                .roles("estagiario").build();
+
+        UserDetails admin = User.withUsername("sky")
+                .password(passwordEncoder().encode("net"))
+                .roles("fodão","fodãoSPlus").build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/login*").permitAll()
+                .antMatchers("/Principal.xhtml").permitAll()
+                .antMatchers("/todas-solicitacoes").hasRole("fodão")
+                .antMatchers("/*.css").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin()
+                .loginPage("/Login.html")
+                .loginProcessingUrl("/login")
+                .permitAll().and()
+                .logout().permitAll();
+    }
+
 
     @Bean
     public ServletRegistrationBean servletRegistrationBean() {
@@ -42,7 +89,7 @@ public class AplicacaoWeb implements ServletContextAware {
     }
     
     @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
+        @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource datasource() {
         return DataSourceBuilder.create().build();
     }
