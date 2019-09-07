8da31468c0d6d8f1faf453cdf282d713d0eeaeed
Aluno <Aluno@PC-43693>
2019-09-06 21:32:35 -0300

    Nona aula

---
+++ src/main/java/com/github/braully/dws/Usuario.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.braully.dws;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Aluno
+ */
+@Entity
+public class Usuario {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    @Column(unique = true)
+    @Basic      
+    String login;
+    
+    @Basic
+    String senha;
+    
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+}
---
+++ src/main/java/com/github/braully/dws/UsuarioMB.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.braully.dws;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Aluno
+ */
+@Component
+public class UsuarioMB {
+    
+    @Autowired
+    UsuarioDAO usuarioDAO;
+    
+    Usuario usuario = new Usuario();
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    
+    public void salvarUsuario(){
+        usuarioDAO.save(usuario);
+        usuario = new Usuario();
+    }
+    
+}
