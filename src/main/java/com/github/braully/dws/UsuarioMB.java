a6319bad2ca6a7b5091fc89bf6a001dd9be4f14c
Aluno <Aluno@PC-43693>
2019-09-13 21:40:38 -0300

    Decima primeira aula

--- src/main/java/com/github/braully/dws/UsuarioMB.java
+++ src/main/java/com/github/braully/dws/UsuarioMB.java
@@ -5,6 +5,8 @@
  */
 package com.github.braully.dws;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -28,6 +30,7 @@ public class UsuarioMB {
     
     public void salvarUsuario(){
         usuarioDAO.save(usuario);
+        FacesContext . getCurrentInstance(). addMessage( null , new FacesMessage ("Usuário salvo com sucesso"));
         usuario = new Usuario();
     }
     
